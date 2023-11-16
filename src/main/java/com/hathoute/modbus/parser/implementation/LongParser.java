dfbec99812dae032d7d9103362bc4a5ddb507fe1
LaarouchiOussama <laarouchioussama@gmail.com>
2023-11-16 15:53:19 +0100

    adding int64 data format

---
+++ src/main/java/com/hathoute/modbus/parser/implementation/LongParser.java
@@ -0,0 +1,22 @@
+package com.hathoute.modbus.parser.implementation;
+
+import com.hathoute.modbus.exception.ByteLengthMismatchException;
+import com.hathoute.modbus.parser.AbstractParser;
+import com.hathoute.modbus.parser.ByteBufferSupplier;
+import com.hathoute.modbus.parser.Parser;
+
+import java.nio.ByteBuffer;
+
+@Parser(formats = {"long", "int64"}, size = 8)
+public class LongParser extends AbstractParser {
+
+    public LongParser(ByteBufferSupplier supplier) {
+        super(supplier);
+    }
+
+    @Override
+    public long parse(byte[] value) throws ByteLengthMismatchException {
+        ByteBuffer byteBuffer = byteBufferSupplier.get(value);
+        return byteBuffer.getLong();
+    }
+}
